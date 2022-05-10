5bcf359867371f4896011ca21dc582669f836abf
HardSlice <105267941+HardSlice@users.noreply.github.com>
2022-05-10 17:22:50 +0200

    Test= menu déroulant

---
+++ test.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>Menu déroulant test</title>
+  </head>
+  <body>
+	<nav>
+		<ul>
+			<li class="menu"><a href="#">Nom étudiant</a>
+				<ul class="submenu">
+					<li><a href="profil.php">Mon Profil</a></li>
+					<li><a href="deconnexion.html">Se Déconnecter</a></li>
+				</ul>
+			</li>
+			<li class="equipe"><a href="#">Equipe</a>
+				<ul class="subequipe">
+					<li><a href="Mequipe.php">Mon équipe </a></li>
+					<li><a href="Tequipe.php">Trouver une équipe</a></li>
+					<li><a href= "Cequipe.php">Créeer une équipe</a></li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+  </body>
+ </html>
