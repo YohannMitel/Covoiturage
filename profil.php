0246064b38211f7680255e226a4807b9c653e51f
HardSlice <105267941+HardSlice@users.noreply.github.com>
2022-05-10 17:45:40 +0200

    Add files via upload
    mdp clean faut juste rajouter la css pas besoin de la bordure au dessus
    profil : yoyo good luck + css+ bordure

---
+++ profil.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>Mon Profil</title>
+  </head>
+  <body>
+	<h1>Mon Profil</h1>
+	<hr>
+	<h3>Tout ce qui est en H3 faut le faire avec les BDD mon petit yoyo</h3>
+	<h3>Nom, prénom, identifiant, addresse, université, groupe, info voiture</h3>
+	<hr>
+	<h4>Mot de passe</h4>
+	<p> Pour changer de mot de passe veuillez cliquer ici : <a href="mdp.php">changer de mot de passe</a>.</p>
+  </body>
