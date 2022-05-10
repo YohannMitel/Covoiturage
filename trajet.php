72c0e9cf3452a78e7ee4001004173ccd29c411b8
Clem3910 <105267939+Clem3910@users.noreply.github.com>
2022-05-10 16:52:30 +0200

    Add files via upload

---
+++ trajet.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>    <meta charset="utf-8">
+    <link rel="stylesheet" href="trajet.css">
+    <title></title>
+  </head>
+  <body>
+    <ul id="nav">
+      <li><a href="index.php">Accueil</a></li>
+      <li><a href="#">J'emmene</a></li>
+      <li><a href="trajet.php">Les trajets</a></li>
+  </ul>
+  <div class="center">
+    <h1>Veuillez entrer un trajet</h1>
+    <form action="trajet" method="post">
+    <div class="texte">
+      <input type="text" required>
+      <span></span>
+      <label>Lieu de départ</label>
+    </div>
+    <div class="texte">
+      <input type="text" required>
+      <span></span>
+      <label>Lieu d'arrivée</label>
+    </div>
+    <div class="texte">
+      <input type="date" required>
+      <span></span>
+      <label>Date</label>
+    </div>
+    <div class="texte">
+      <input type="time" required>
+      <span></span>
+      <label>Horaire de départ</label>
+    </div>
+    <div class="texte">
+      <input type="time" required>
+      <span></span>
+      <label>Horaire de retour</label>
+    </div>
+    
+    <input type="submit" value="Confirmer">
+    </form>
+  </div>
+
+  </body>
+</html>
