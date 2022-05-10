c82225ce82da4b363fc8b3480909fcb405277600
Clem3910 <105267939+Clem3910@users.noreply.github.com>
2022-05-10 17:45:18 +0200

    Add files via upload

---
+++ deco.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="deco.css">
+    <title>Déconnexion</title>
+  </head>
+
+  <body>
+    <div class="center">
+      <h1>Vous avez bien été déconnecté</h1>
+    	<p>Vous avez bien été déconnecté de votre compte Covoit'Udiant.<br><br>
+    	En espérant vous revoir dans très peu de temps !!
+    	<br><br>
+    	A bientôt !
+    	</p>
+      <button type="button" name="button" onclick=window.location.href='connexion.html'>Reconnexion</button>
+    </div>
+
+  </body>
