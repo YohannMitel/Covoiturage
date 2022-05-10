a4145654ccca3e83fa3ca50a862cdae15049dfac
Yohann MITEL <95537983+YohannMitel@users.noreply.github.com>
2022-05-10 16:05:19 +0200

    Update enmene.php

--- jemmene.php
+++ jemmene.php
@@ -12,45 +12,93 @@
   </ul>
   <div class="center">
     <h1>Veuillez entrer un trajet</h1>
-    <form action="trajet" method="post">
+    <form action="enmene.php" method="get">
     <div class="texte">
-      <input type="text" required>
+      <input type="text" name="arrive" required>
       <span></span>
-      <label>Lieu de départ</label>
+      <label for="arrive">Lieu d'arrivée</label>
     </div>
     <div class="texte">
-      <input type="text" required>
+      <input type="date" name="date" required>
       <span></span>
-      <label>Lieu d'arrivée</label>
+      <label for ="date">Date</label>
     </div>
     <div class="texte">
-      <input type="date" required>
+      <input type="time" name="daller" required>
       <span></span>
-      <label>Date</label>
+      <label for = "daller">Horaire de départ</label>
     </div>
     <div class="texte">
-      <input type="time" required>
+      <input type="time" name="dretour" required>
       <span></span>
-      <label>Horaire de départ</label>
+      <label for="dretour">Horaire de retour</label>
     </div>
     <div class="texte">
-      <input type="time" required>
+      <input type="text" name="immatriculation" required>
       <span></span>
-      <label>Horaire de retour</label>
+      <label for="immatriculation">Immatriculation du véhicule</label>
+    </div>
+    <div>
+          <input type="radio" id="A" name="typet" value="A" required>
+          <label for="A">Aller</label>
+        </div>
+        <div>
+          <input type="radio" id="R" name="typet" value="R" required>
+          <label for="R">Retour</label>
+        </div>
+        <div>
+          <input type="radio" id="AR" name="typet" value="AR" required>
+          <label for="AR">Aller et retour</label>
+        </div>
     </div>
     <div class="texte">
-      <input type="text" required>
+      <input type="number" name="nbplace" min="1">
       <span></span>
-      <label>Immatriculation du véhicule</label>
-    </div>
-    <div class="texte">
-      <input type="number" min="1"required>
-      <span></span>
-      <label>Nombre de places disponibles</label>
+      <label for ="nbplace">Nombre de places disponibles (facultatif)</label>
     </div>
     <input type="submit" value="Confirmer">
     </form>
   </div>
-
   </body>
+
+    <?php
+    require('config.php');
+    session_start();
+    echo $_SESSION['nom'];
+    if (
+    (!empty($_GET['arrive']))
+    && (!empty($_GET['date']))
+    && (!empty($_GET['daller']))
+    && (!empty($_GET['dretour']))
+    && (!empty($_GET['immatriculation']))
+    && (!empty($_GET['typet']))
+    ){
+
+        $session =  $_SESSION['nom'];
+        $depart = mysqli_query($conn,"SELECT domicile
+        FROM etudiant 
+        WHERE  nom>='$session'");
+        echo "iciiiiiiii";
+        $row=mysqli_fetch_assoc ($depart);
+        $rowval = $row['domicile'];
+        print_r($rowval);
+        $arrive = $_GET['arrive'];
+        $date = $_GET['date'];
+        $daller = $_GET['daller'];
+        $dretour = $_GET['dretour'];
+        $immatriculation = $_GET['immatriculation'];
+        $typet = $_GET['typet'];
+        $nbplace = $_GET['typet'];
+        $sql="INSERT INTO  trajet
+        VALUES (NULL,'0','0', '$rowval', '$arrive', '$daller', '$dretour', '$date','$typet')";
+        
+        if ($conn->query($sql) === TRUE) {
+          echo "Update successfully";
+        } else {
+          echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        $conn->close();
+    }
+      
+      ?>
 </html>
