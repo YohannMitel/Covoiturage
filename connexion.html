d9e0c6d51b58a6183f6791757cedcd3159795369
Clem3910 <105267939+Clem3910@users.noreply.github.com>
2022-05-10 08:11:26 +0200

    Add files via upload

---
+++ connexion.html
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Page de connexion</title>
+    <link rel="stylesheet" href="connexion.css">
+    <script src="" charset="utf-8"></script>
+  </head>
+  <body>
+    <div class="center">
+      <h1>Veuillez vous connecter</h1>
+      <form method="post">
+        <div class="texte">
+          <input type="text" required>
+          <span></span>
+          <label>Identifiant</label>
+        </div>
+        <div class="texte">
+          <input type="password" required>
+          <span></span>
+          <label>Mot de passe</label>
+        </div>
+        <input type="submit" value="Se connecter">
+      </form>
+    </div>
+  </body>
+</html>
---
+++ sae.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="sae.css">
+    <title></title>
+  </head>
+  <body>
+    <ul id="nav">
+      <li><a href="sae.html">Accueil</a></li>
+      <li><a href="#">J'emmene</a></li>
+      <li><a href="#">Les trajets</a></li>
+      <li style="float:right"><a class="active" href="#about">Se connecter</a></li>
+  </ul>
+  <br><br><br><br>
+  <h1>Bienvenue sur Convoitudiant</h1>
+  <br><br>
+  <h2 id="p1">Une fois Convoitudiant,<br>Toujours Convoitudiant</h2>
+
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  <h4>Nos partenaires</h4>
+
+  <button type="button" name="button" onclick=window.location.href='http://monsite.fr'>A propos</button>
+  </body>
+</html>
