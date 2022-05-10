dbb8044d2e5bba3b5a8a627e67d680bff5919196
HardSlice <105267941+HardSlice@users.noreply.github.com>
2022-05-10 14:00:02 +0200

    équipe.php

---
+++ equipe.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="sae.css">
+    <title>Création d'équipe</title>
+  </head>
+  <body>
+	<h1>Création d'équipe</h1>
+	 <div class="texte">
+      <input type="number" min="1"required>
+      <span></span>
+      <label>Nombre de places disponibles</label>
+    </div>
+  </body>
+ </html>
