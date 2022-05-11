21c779b522c3791ba90f6beebf60121d38950fc7
Clem3910 <105267939+Clem3910@users.noreply.github.com>
2022-05-11 13:05:57 +0200

    Add files via upload

---
+++ listetrajets.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<link rel="stylesheet" href="listetrajets.css">
+</head>
+<body>
+  <div class="scroll-bg">
+    <h1>Liste des trajets</h1>
+    <div class="scroll-div">
+      <div class="scroll-object">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Lieu Domicile</th>
+              <th>Lieu de travail</th>
+              <th>Heure de départ</th>
+              <th>Heure d'arrivée</th>
+              <th>Date</th>
+              <th>Identifiant equipage</th>
+              <th>Immatriculation vehicule</th>
+              <th>Type</th>
+              <th>Supprimer</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
